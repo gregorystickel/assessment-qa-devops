414931ccb03592d7680022a622ee5d81bbba5e17
Greg Stickel <gregory.stickel@live.com>
2022-10-21 12:50:14 -0400

    Updated 12:49pm

--- functions.test.ts
+++ functions.test.ts
@@ -3,10 +3,17 @@ const {shuffleArray} = require('./utils')
 describe('shuffleArray should', () => {
     test('Does shuffle array receive an array', () => {
         let arr = [35, 40, 41, 42];
-        expect(shuffleArray(arr)).toContain([35, 40, 41,42])
+        expect(shuffleArray(arr)).toEqual(expect.arrayContaining(arr))
     })
-    // test('Does shuffle array return an array', () => {
-    //    expect(shuffleArray([35, 40, 41, 42]).toContain([35, 40, 41, 42])
-    // })
+
+    test('Does shuffle array return an array', () => {
+        expect(shuffleArray([35, 40, 41, 42]).toEqual([35, 40, 41, 42]))
+    })
+
+    test('shuffle array has been called', () => {
+        
+        shuffleArray([35, 40, 41, 42]);
+        expect(shuffleArray).toHaveBeenCalled();
+    })    
     
 })
